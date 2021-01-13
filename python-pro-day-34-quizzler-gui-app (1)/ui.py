c5b8cdaadc223173663607df556638f80161e6a2
SamroodAli <samroodali@live.com>
2021-01-13 17:18:23 +0530

    UI: Widgets styling

--- python-pro-day-34-quizzler-gui-app (1)/ui.py
+++ python-pro-day-34-quizzler-gui-app (1)/ui.py
@@ -1,6 +1,8 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, PhotoImage, Canvas
 """UI Interface, Theme and colors"""
 THEME_COLOR = "#375362"
+SCORE_FONT = ("Ariel", 20, "italic")
+QUESTION_FONT = ("Ariel", 15, "italic")
 
 
 class QuizInterface:
@@ -9,22 +11,33 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(bg=THEME_COLOR)
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+        self.window.minsize(height=250, width=300)
 
         # Label = Grid system
-        score_label = Label(text=f"Score: 0", padx=20, pady=20)
-        score_label.grid(row=0, column=1)
+        self.score_label = Label(text=f"Score: 0", fg="white", bg=THEME_COLOR, font=SCORE_FONT)
+        self.score_label.grid(row=0, column=1)
 
         # Question Label
-        question_label = Label(text="Question text here", padx=20, pady=20)
-        question_label.grid(row=1, column=0, columnspan=2)
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some question text",
+            fill=THEME_COLOR,
+            font=QUESTION_FONT
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        # PhotoImages
+        true_image = PhotoImage(file="images/true.png")
+        false_image = PhotoImage(file="images/false.png")
 
         # Buttons True or False
-        true_button = Button(text="Click Me")
-        true_button.grid(row=2, column=0)
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
 
-        false_button = Button(text="Click Me")
-        false_button.grid(row=2, column=1)
-
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
